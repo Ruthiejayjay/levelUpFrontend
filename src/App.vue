c2e8609aae9851c00cc8a1b109db22dc4d5a509f
Ruth James <84787216+Ruthiejayjay@users.noreply.github.com>
2022-08-09 18:44:15 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <Navigation class="" />
+  <Cart class="xl:m-20" />
+  <Footer />
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navigation from '@/components/Navigation.vue'
+import Cart from '@/components/Cart.vue'
+import Footer from '@/components/Footer.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Navigation,
+    Footer,
+    Cart
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
